cc476931a67a112fa7a3006f823c777a5227ab3f
abtinJA42 <abtin.jahanandish99@gmail.com>
2023-10-02 10:45:46 +0330

    added new functionalities

--- js/script.js
+++ js/script.js
@@ -2,6 +2,51 @@ const global = {
     currentPage : window.location.pathname
 }
 
+async function displayPopularMovies() { 
+  const { results } = await fetchAPIData('movies/popular')
+ 
+  results.forEach( movie => {
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+   ${
+    movie.poster_path
+    ? `<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+    />` 
+      : `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${movie.title}"
+    />`
+    }
+    </a>
+    <div class="card-body">
+    <h5 class="card-title">${movie.title}</h5>
+    <p class="card-text">
+    <small class="text-muted">Release: ${movie.release_date}</small>
+    </p>
+    </div>
+    `
+
+    document.querySelector('#popular-movies').appendChild(div)
+  })
+
+}
+
+// fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'b2da565148e4104898e83a8d542e73d3'
+  const API_URL = 'https://api.themoviedb.org/3/'
+
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`)
+  const data = await response.json()
+  return data
+}
+
 // highligh active link
 function highlighActiveLink() {
   const links = document.querySelectorAll('.nav-link')
@@ -17,15 +62,15 @@ function init() {
     switch(global.currentPage){
       case  '/' :
         case '/index.html':
-        console.log('Home');
+          displayPopularMovies()
         break
       case '/shows.html' :
         console.log('show');
         break
-      case '/movie-detailes.html':  
+      case '/movie-details.html':  
       console.log('movie-detailes');
       break
-      case '/tv-detailes.html':
+      case '/tv-details.html':
         console.log('tv detailes');
         break
         case '/search.html':
