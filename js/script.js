8dbb4dc4ccdea7873c18385ff953723287d128ff
abtinJA42 <abtin.jahanandish99@gmail.com>
2023-10-02 11:12:37 +0330

    added new functionality

--- js/script.js
+++ js/script.js
@@ -36,13 +36,50 @@ async function displayPopularMovies() {
   });
 }
 
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `;
+
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
 // fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = 'b2da565148e4104898e83a8d542e73d3'
   const API_URL = 'https://api.themoviedb.org/3/'
 
+   showSpinner()
+
   const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`)
   const data = await response.json()
+  hideSpinner()
   return data
 }
 
@@ -56,6 +93,14 @@ function highlighActiveLink() {
   })
 }
 
+function showSpinner() { 
+    document.querySelector('.spinner').classList.add('show')
+}
+
+function hideSpinner() { 
+  document.querySelector('.spinner').classList.remove('show')
+}
+
 // init app
 function init() {
     switch(global.currentPage){
@@ -64,7 +109,7 @@ function init() {
           displayPopularMovies()
         break
       case '/shows.html' :
-        console.log('show');
+        displayPopularShows()
         break
       case '/movie-details.html':  
       console.log('movie-detailes');
