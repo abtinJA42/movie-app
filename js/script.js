070cc07d7c9ebbcb0ebd056430501d9104514da2
abtinJA42 <abtin.jahanandish99@gmail.com>
2023-10-03 10:08:16 +0330

    added swpier utility to the project

--- js/script.js
+++ js/script.js
@@ -224,6 +224,49 @@ function dsiplayBackgroundImage(type,backgroundPath) {
   }
 }
 
+// display slider movies
+async function displaySlider() {
+   const {results} = await fetchAPIData('movie/now_playing')
+     results.forEach((movie)=> {
+      const div = document.createElement('div')
+      div.classList.add('swiper-slide')
+      div.innerHTML = `
+       <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+      </h4>
+      `
+      document.querySelector('.swiper-wrapper').appendChild(div)
+
+      initSwiper()
+     })
+}
+
+
+function initSwiper() { 
+  const swiper = new Swiper('.swiper' , { 
+    slidesPerView : 1 ,
+    spaceBetween : true ,
+    freeMode : true ,
+    loop : true ,
+    autoplay: {
+      delay :4000 ,
+      disableOnInteraction : false
+    } ,
+    breakpoints: {
+      500: {
+        slidesPerView: 2
+      } ,
+       700: {
+        slidesPerView: 3
+      } , 1200: {
+        slidesPerView: 4
+      } ,
+    }
+  })
+}
 
 // fetch data from TMDB API
 async function fetchAPIData(endpoint) {
@@ -265,7 +308,8 @@ function init() {
     switch(global.currentPage){
       case  '/' :
         case '/index.html':
-          displayPopularMovies()
+          displaySlider()  
+        displayPopularMovies()
         break
       case '/shows.html' :
         displayPopularShows()
